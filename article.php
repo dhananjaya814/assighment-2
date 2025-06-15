5ace5d7c77d69b5979fb03086785a6d87704f440
T.Dhananjaya <dananjayakumara527@gmail.com>
2025-06-15 11:13:09 +0530

    Add files via upload

---
+++ article.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>submitted</title>
+    <link rel="stylesheet" href="article.css">
+</head>
+<body>
+    <div id="container">
+     <?php include("header.php"); ?>
+    </div> 
+
+       <h1><b><i><ins>Thank you for join with us</b></i></ins></h1>
+
+       <p><b>Please provide us with any information you know about the
+       </b><br>
+       <br><br>
+        <img src="download (2).jpg" alt="Sri Dalada Perahera" width="150" height="100">
+        <img src="download (1).jpg" alt="Sri Dalada maligaya" width="200" height="100">
+        <img src="images.jpg" alt="Sri Dalada Perahera" width="150" height="100">
+        <img src="download.jpg" alt="Sri Dalada maligaya" width="200" height="100">
+            <ul>
+                <li> Past activities....</li>
+                <li> Ceremonies....</li>
+                <li>History of the procession....</li>
+                <li> Royal lineage....</li>
+                <li> Books....</li>
+                <li> Customs....</li>
+                <li> Traditions....</li>
+                <li> History.... </li>
+                <li>Other....</li>
+            </ul>
+                <ins>Of the Temple of the Tooth Relic and the procession.<strong> We will do our best to conduct research and update this website with accurate information.</ins></strong>
+        </p>
+
+        <img src="view.jpg" alt="View" width="150" height="100">
+        <img src="3.jpg" alt="Thammettam" width="150" height="100">
+        <img src="eththu.jpg" alt="Eththu" width="300" height="100">
+        <img src="night.jpg" alt="Night view" width="150" height="100">
+        <img src="2.jpg" alt="Dale eththu" width="150" height="100">
+        <img src="1.jpg" alt="Perahera eththu" width="150" height="100">
+        
+       <h2>Enter your topic and information related to the topic in the form below.</h2>
+       
+       <h3>[Select the relevant topic above].</h3>
+       <div id="form"> 
+
+       <form action=submitted.php method="post">
+
+         <label>Date:</label>
+         <input type="text" name="date" required ><br><br>
+    
+         <label>Topic:</label>
+         <input type="text" name="topic" required ><br><br>
+
+         <label>Data & Information:</label>
+         <input type="text" name="data & information" required ><br><br><br>
+
+         <input type="submit" value="CLICK IN">
+
+       </form>
+       </div><br><br>
+
+    <img src="collection.jpg" alt="Sri Dalada perahera" width="1000" height="400">
+
+    <div id="content p-[20px]">
+     <?php include("footer.php"); ?>
+    </div> 
+</body>
+</html>
---
+++ submitted.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>END blog</Em>nd</title>
+    <link rel="stylesheet" href="sub.css">
+    
+</head>
+<body>
+
+ <?php include("header.php"); ?>
+
+   <h1><ins><i>Thank you<i> for join with our web site</ins></h1>
+
+    <img src="OLD.jpg" alt="Final-2 Pic" width="1300" height="600">
+
+   <h2>NAMO BUDHDHAYA!!!</h2>
+
+ <?php include("footer.php"); ?>
+</body>
+</html>
